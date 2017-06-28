3d5c5f9048f7950d4f0d71fcc23bbfcfd663b5d2
Igor Beliakovski <nuevo-design@mail.ru>
2017-06-29 00:03:29 +0300

    Some fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,52 @@
 "use strict";
 
-var mainNav = document.querySelector('.main-nav');
-var subMenuOpenLink = mainNav.querySelector('.main-nav__link');
-var subMenuElem = mainNav.querySelector('.main-nav__submenu');
+(function() {
 
-var mouseClickHandler = function (e) {
-  e.preventDefault();
-  subMenuElem.classList.toggle('main-nav__submenu--open');
-};
+  var mainNav = document.querySelector('.main-nav');
+  var subMenuOpenLink = mainNav.querySelector('.main-nav__link');
+  var subMenuElem = mainNav.querySelector('.main-nav__submenu');
 
-var toggleMenu = function (e) {
-  if (subMenuElem.classList.contains('main-nav__submenu--open')) {
-    subMenuElem.classList.remove('main-nav__submenu--open');
-  }
-  subMenuOpenLink.addEventListener('click', mouseClickHandler);
-};
+  var mouseClickHandler = function (e) {
+    e.preventDefault();
+    subMenuElem.classList.toggle('main-nav__submenu--open');
+  };
 
-window.addEventListener('DOMContentLoaded', function (e) {
-  toggleMenu(e);
-});
+  var toggleMenu = function (e) {
+    if (subMenuElem.classList.contains('main-nav__submenu--open')) {
+      subMenuElem.classList.remove('main-nav__submenu--open');
+    }
+    subMenuOpenLink.addEventListener('click', mouseClickHandler);
+  };
+
+  window.addEventListener('DOMContentLoaded', function (e) {
+    toggleMenu(e);
+  });
+
+  window.addEventListener('keydown', function (e) {
+    if(window.utils.isDeactivateEvent(e) && subMenuElem.classList.contains('main-nav__submenu--open')) {
+      subMenuElem.classList.remove('main-nav__submenu--open');
+    }
+  } );
+
+})();
+
+window.utils = (function () {
+
+  var ENTER_KEY_CODE = 13;
+  var ESCAPE_KEY_CODE = 27;
+
+  var isKeyboardEvent = function (e) {
+    return typeof e.keyCode !== 'undefined';
+  };
+
+  return {
+    isActivateEvent: function (e) {
+      return isKeyboardEvent(e) && e.keyCode === ENTER_KEY_CODE;
+    },
+
+    isDeactivateEvent: function (e) {
+      return isKeyboardEvent(e) && e.keyCode === ESCAPE_KEY_CODE;
+    }
+  };
+
+})();
